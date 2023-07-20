63aad364141c6b239f139ea9b4b1af36cc0e41af
Nguyen Anh <nguyenanh.nda@gmail.com>
2023-07-20 18:03:48 +0900

    Update

--- training.py
+++ training.py
@@ -15,12 +15,13 @@ def tx():
     T2 = SuchTree('data/plant-pollinators/' + name + '/animal.tree')
     links = pd.read_csv('data/plant-pollinators/' + name + '/' + name + '_links.csv', index_col=0)
 
-
-
     edges, edge_features, x, n1 = create_graphpair(T1, T2, links)
 
-    tMatchingModel = TMatching(FLAGS.EDGE_FEATURE_DIM, FLAGS.EDGE_FEATURE_EMBEDDING_DIM, FLAGS.NODE_FEATURE_EMBEDDING_DIM)
+    tMatchingModel = TMatching(FLAGS.EDGE_FEATURE_DIM, FLAGS.EDGE_FEATURE_EMBEDDING_DIM,
+                               FLAGS.NODE_FEATURE_EMBEDDING_DIM)
     x = tMatchingModel(x, edges, edge_features)
     print(x.shape, x)
+
+
 if __name__ == "__main__":
     tx()
